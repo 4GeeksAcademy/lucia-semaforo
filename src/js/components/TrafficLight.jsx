3b4d0ad4288424b2f4c6980b85276ae61b44b5f1
Lucia <lucia.casasco02@gmail.com>
2025-09-13 13:39:45 +0000

    Lucia Traffic Light

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+    // Hook useState para manejar qué luz está brillando
+    const [color, setColor] = useState("red");
+
+    return (
+        <div className="traffic-light">
+            <div 
+                className={`light red ${color === "red" ? "selected" : ""}`}
+                onClick={() => setColor("red")}
+            ></div>
+            <div 
+                className={`light yellow ${color === "yellow" ? "selected" : ""}`}
+                onClick={() => setColor("yellow")}
+            ></div>
+            <div 
+                className={`light green ${color === "green" ? "selected" : ""}`}
+                onClick={() => setColor("green")}
+            ></div>
+        </div>
+    );
+};
+
+export default TrafficLight;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
-// index.css'
 import '../styles/index.css'
 
-// components
-import Home from './components/Home';
+import TrafficLight from './components/TrafficLight';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <TrafficLight />
+    </React.StrictMode>
 )
+ 
